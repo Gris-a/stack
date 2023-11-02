acfc5baeedb436d2adfc2af60eb84a86c5f9407f
Завьялов Григорий Аркадьевич <zavialov.ga@phystech.edu>
2023-11-02 19:50:14 +0300

    Update log.h

--- include/log.h
+++ include/log.h
@@ -9,6 +9,25 @@
 
 #include <stdio.h>
 
+
+#ifndef LOG_CPP
+extern FILE *LOG_FILE;
+#endif
+
+/**
+ * @brief Macro for logging.
+ */
+#define LOG(...) fprintf(LOG_FILE, __VA_ARGS__)
+
+/**
+ * @brief Macro for checking condition and writing result into log file.
+ */
+#define ASSERT(condition, action)   if(!(condition))\
+                                    {\
+                                        LOG("%s:%s:%d\n"\
+                                            "\tAssertion cathced at ASSERT(" #condition "...);\n", __FILE__, __PRETTY_FUNCTION__, __LINE__);\
+                                        action;\
+                                    }
 /**
  * @brief Opens log file in "w" mode.
  * @return FILE* pointer to FILE for logging.
